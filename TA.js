bbbef8b43c36f51703b8debd8b890cb122ca67b0
vladpostu <vladposth@gmail.com>
2022-02-07 10:42:45 +0100

    TA.js: commented

--- TA.js
+++ TA.js
@@ -1,15 +1,25 @@
+/**
+ * TA - Typing Animation Library
+ * Javascript part
+ * 
+ * @author Vlad Postu
+ */
 
 
-
-
+// TA class containing the methods needed for the animation
 class TA {
     
+    // state containg the animation attributes
     state = {
         htmlEl: '',
         minDelay: 0, 
         maxDelay: 0,
     }
 
+    /**
+     * Constructor
+     * @param {object} state 
+     */
     constructor(state){
         this.state = {
             htmlEl: document.querySelector(state.htmlEl),
@@ -18,7 +28,13 @@ class TA {
         }
     }
 
-    // recursive typing animation forward
+    /**
+     * typingForward method
+     * Based on the base form element in the state the function gradually add letters until it has reached the entered parameter
+     * i param must be 0 at the first function call
+     * @param {String} finalForm 
+     * @param {number} i 
+     */
     typingForward(finalForm, i) {
         let duration = this.getRandomNumber(this.state.minDelay, this.state.maxDelay);
 
@@ -30,6 +46,11 @@ class TA {
         }
     }
 
+    /**
+     * typingBackward methdo
+     * Based on the base form element in the state the function gradually remove letters until it has reached the entered parameter
+     * @param {String} finalForm 
+     */
     typingBackward(finalForm) {
         let duration = this.getRandomNumber(this.state.minDelay, this.state.maxDelay);
     
@@ -40,6 +61,13 @@ class TA {
         setTimeout(() => this.typingBackward(finalForm), duration);
     }
     
+    /**
+     * getRandomNumber method
+     * Generate random number using the two parameters as extremes
+     * @param {number} min 
+     * @param {number} max 
+     * @returns random generated number
+     */
     getRandomNumber(min, max) {
         return Math.floor(Math.random() * max) + min;
     }
